a67e59cf1dd020d7a1a94a3e923ff12385162089
Luana da Rosa <lrosa.silva@outlook.com>
2021-12-17 11:26:19 -0300

    adiciona autocommit e rollback

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -7,9 +7,27 @@ public class TestaInsercaoComParametro {
         ConnectionFactory factory = new ConnectionFactory();
         Connection connection = factory.recuperarConexao();
 
-        PreparedStatement stm =
-                connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        connection.setAutoCommit(false);
 
+        try {
+            PreparedStatement stm =
+                    connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            adicionarVariavel("Smart TV", "45 polegadas", stm);
+            adicionarVariavel("Radio", "Radio de bateria", stm);
+
+            connection.commit();
+            stm.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("ROLLBACK executado");
+            connection.rollback();
+        }
+
+        connection.close();
+    }
+
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
         stm.setString(1, nome);
         stm.setString(2, descricao);
 
@@ -20,6 +38,5 @@ public class TestaInsercaoComParametro {
             Integer id = rst.getInt(1);
             System.out.println("O ID criado foi: "+id);
         }
-
     }
 }
